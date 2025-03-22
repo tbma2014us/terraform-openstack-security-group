bdddaa73cabd94dd8867f351881d48a91368101d
Alex Zimin <alex.zimin@gmail.com>
2025-03-22 15:44:13 -0700

    Rewrite for modern TF syntax

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.8.0"
   required_providers {
     openstack = {
       source = "terraform-provider-openstack/openstack"
+      version = "~> 1.53.0"
     }
   }
 }
 
 resource "random_id" "this" {
-  count       = var.create && var.use_name_prefix && var.name_prefix == "" ? 1 : 0
+  count    = var.create && var.use_name_prefix && var.name_prefix == "" ? 1 : 0
   byte_length = 8
 }
 
@@ -19,8 +20,10 @@ locals {
   this_sg_id = concat(
     openstack_networking_secgroup_v2.this.*.id,
     [""],
-  )[0]
-  this_sg_name = var.use_name_prefix ? (var.name_prefix == "" ? "${random_id.this[0].hex}-${var.name}" : "${var.name_prefix}-${var.name}") : var.name
+  )[
+  0
+  ]
+  this_sg_name = var.use_name_prefix ? (var.name_prefix == "" ? join( "-", [random_id.this[0].hex, var.name]) : join( "-", [var.name_prefix,var.name])): var.name
 }
 
 
@@ -45,55 +48,55 @@ resource "openstack_networking_secgroup_v2" "this" {
 # Security group rules
 ######################
 locals {
-  ingress_rules_ipv4 = [for r in var.ingress_rules_ipv4 : merge(r, { direction = "ingress", ethertype = "IPv4" })]
-  ingress_rules_ipv6 = [for r in var.ingress_rules_ipv6 : merge(r, { direction = "ingress", ethertype = "IPv6" })]
-  ingress_rules      = concat(local.ingress_rules_ipv4, local.ingress_rules_ipv6, [])
+  ingress_rules_ipv4 = { for r in var.ingress_rules_ipv4 : sha1(jsonencode(r)) => merge(r, { direction = "ingress", ethertype = "IPv4" }) }
+  ingress_rules_ipv6 = { for r in var.ingress_rules_ipv6 : sha1(jsonencode(r)) => merge(r, { direction = "ingress", ethertype = "IPv6" }) }
+  ingress_rules = merge(local.ingress_rules_ipv4, local.ingress_rules_ipv6)
 
-  ingress_rules_both_ipv6 = [for r in var.ingress_rules : merge(r, { direction = "ingress", ethertype = "IPv6" })]
-  ingress_rules_both_ipv4 = [for r in var.ingress_rules : merge(r, { direction = "ingress", ethertype = "IPv4" })]
-  ingress_rules_both      = concat(local.ingress_rules_both_ipv4, local.ingress_rules_both_ipv6, [])
+  ingress_rules_both_ipv6 = { for r in var.ingress_rules : sha1(jsonencode(r)) => merge(r, { direction = "ingress", ethertype = "IPv6" }) }
+  ingress_rules_both_ipv4 = { for r in var.ingress_rules : sha1(jsonencode(r)) => merge(r, { direction = "ingress", ethertype = "IPv4" }) }
+  ingress_rules_both = merge(local.ingress_rules_both_ipv4, local.ingress_rules_both_ipv6)
 
-  egress_rules_ipv4 = [for r in var.egress_rules_ipv4 : merge(r, { direction = "egress", ethertype = "IPv4" })]
-  egress_rules_ipv6 = [for r in var.egress_rules_ipv6 : merge(r, { direction = "egress", ethertype = "IPv6" })]
-  egress_rules      = concat(local.egress_rules_ipv4, local.egress_rules_ipv6, [])
+  egress_rules_ipv4 = { for r in var.egress_rules_ipv4 : sha1(jsonencode(r)) => merge(r, { direction = "egress", ethertype = "IPv4" }) }
+  egress_rules_ipv6 = { for r in var.egress_rules_ipv6 : sha1(jsonencode(r)) => merge(r, { direction = "egress", ethertype = "IPv6" }) }
+  egress_rules = merge(local.egress_rules_ipv4, local.egress_rules_ipv6)
 
-  egress_rules_both_ipv6 = [for r in var.egress_rules : merge(r, { direction = "egress", ethertype = "IPv6" })]
-  egress_rules_both_ipv4 = [for r in var.egress_rules : merge(r, { direction = "egress", ethertype = "IPv4" })]
-  egress_rules_both      = concat(local.egress_rules_both_ipv4, local.egress_rules_both_ipv6, [])
+  egress_rules_both_ipv6 = { for r in var.egress_rules : sha1(jsonencode(r)) => merge(r, { direction = "egress", ethertype = "IPv6" }) }
+  egress_rules_both_ipv4 = { for r in var.egress_rules : sha1(jsonencode(r)) => merge(r, { direction = "egress", ethertype = "IPv4" }) }
+  egress_rules_both = merge(local.egress_rules_both_ipv4, local.egress_rules_both_ipv6)
 
-  rules_ipv4 = [for r in var.rules_ipv4 : merge(r, { ethertype = "IPv4" })]
-  rules_ipv6 = [for r in var.rules_ipv6 : merge(r, { ethertype = "IPv6" })]
+  rules_ipv4 = {for r in var.rules_ipv4 : sha1(jsonencode(r)) =>  merge(r, { ethertype = "IPv4" })}
+  rules_ipv6 = {for r in var.rules_ipv6 : sha1(jsonencode(r)) =>  merge(r, { ethertype = "IPv6" })}
 
-  rules_both_ipv4 = [for r in var.rules : merge(r, { ethertype = "IPv4" })]
-  rules_both_ipv6 = [for r in var.rules : merge(r, { ethertype = "IPv6" })]
-  rules_both      = concat(local.rules_both_ipv4, local.rules_both_ipv6, [])
+  rules_both_ipv4 = {for r in var.rules : sha1(jsonencode(r)) =>  merge(r, { ethertype = "IPv4" })}
+  rules_both_ipv6 = {for r in var.rules : sha1(jsonencode(r)) =>  merge(r, { ethertype = "IPv6" })}
+  rules_both      = merge(local.rules_both_ipv4, local.rules_both_ipv6)
 
-  rules = concat(local.rules_both, local.rules_ipv4, local.rules_ipv6, local.egress_rules_both, local.egress_rules, local.ingress_rules_both, local.ingress_rules, [])
+  rules = merge(local.rules_both, local.rules_ipv4, local.rules_ipv6, local.egress_rules_both, local.egress_rules, local.ingress_rules_both, local.ingress_rules)
 }
 
 resource "openstack_networking_secgroup_rule_v2" "rules" {
-  count             = var.create ? length(local.rules) : 0
+  for_each          = var.create ? local.rules : 0
   security_group_id = local.this_sg_id
-  direction         = lookup(local.rules[count.index], "direction", null)
+  direction         = lookup(each.value, "direction", null)
 
-  port_range_min = lookup(local.rules[count.index], "port", lookup(local.rules[count.index], "port_range_min", null))
-  port_range_max = lookup(local.rules[count.index], "port", lookup(local.rules[count.index], "port_range_max", null))
-  protocol       = lookup(local.rules[count.index], "protocol", null)
-  ethertype      = lookup(local.rules[count.index], "ethertype", null)
-  description    = lookup(local.rules[count.index], "description", null)
+  port_range_min   = lookup(each.value, "port", lookup(each.value, "port_range_min", null))
+  port_range_max   = lookup(each.value, "port", lookup(each.value, "port_range_max", null))
+  protocol         = lookup(each.value, "protocol", null)
+  ethertype        = lookup(each.value, "ethertype", null)
+  description      = lookup(each.value, "description", null)
   remote_ip_prefix = (
-    lookup(local.rules[count.index], "remote_ip_prefix", null) == null
-    ? (
-      lookup(local.rules[count.index], "ethertype", null) == "IPv6"
-      ? (lookup(local.rules[count.index], "remote_group_id", null) == null ? var.default_ipv6_remote_ip_prefix : null)
-      : (lookup(local.rules[count.index], "remote_group_id", null) == null ? var.default_ipv4_remote_ip_prefix : null)
-    )
-    : lookup(local.rules[count.index], "remote_ip_prefix", null)
+  lookup(each.value, "remote_ip_prefix", null) == null
+  ? (
+  lookup(each.value, "ethertype", null) == "IPv6"
+  ? (lookup(each.value, "remote_group_id", null) == null ? var.default_ipv6_remote_ip_prefix : null)
+  : (lookup(each.value, "remote_group_id", null) == null ? var.default_ipv4_remote_ip_prefix : null)
+  )
+  : lookup(each.value, "remote_ip_prefix", null)
   )
   remote_group_id = (
-    lookup(local.rules[count.index], "remote_group_id", null) == "@self"
-    ? local.this_sg_id
-    : lookup(local.rules[count.index], "remote_group_id", null)
+  lookup(each.value, "remote_group_id", null) == "@self"
+  ? local.this_sg_id
+  : lookup(each.value, "remote_group_id", null)
   )
 }
 
